0cf973c1234e22720098df52499c63a404281d68
Tu NombreAndy zuiga <andy.zuniga.williams@gmail.com>
2025-04-14 10:43:53 -0400

    Reemplazado bcrypt por bcryptjs para compatibilidad con Render

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ app.use((err, req, res, next) => {
 
 
 // Configurar el puerto
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
